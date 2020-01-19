82a36f556f4af3f3ad469a46641c5b27a4933f99
Edenilton <edeniltonborba.adm@gmail.com>
2020-01-19 09:12:44 +0100

    update

--- db/connection.js
+++ db/connection.js
@@ -3,17 +3,12 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  // Your username
   user: "root",
-  // Your password
-  password: "",
+  password: process.env.DB_PASS,
   database: "employees"
 });
 
 connection.connect();
-
-// Setting up connection.query to use promises instead of callbacks
-// This allows us to use the async/await syntax
 connection.query = util.promisify(connection.query);
 
 module.exports = connection;
